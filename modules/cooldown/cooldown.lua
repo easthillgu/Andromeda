245b6e6e1795d349b1cc81a8f2db440443651b8e
easthillgu <58293755@qq.com>
2026-05-23 00:13:26 +0800

    fix(font): 注册 Small 字体(super-effective)，修复 cooldown SetFont nil 参数

--- modules/cooldown/cooldown.lua
+++ modules/cooldown/cooldown.lua
@@ -56,7 +56,7 @@ function COOLDOWN:OnSizeChanged(width, height)
     if fontScale < minScale then
         self:Hide()
     else
-        self.text:SetFont(font, fontScale * fontSize, outline and 'OUTLINE' or nil)
+        self.text:SetFont(font, fontScale * fontSize, outline and 'OUTLINE' or '')
         self.text:SetShadowColor(0, 0, 0, outline and 0 or 1)
         self.text:SetShadowOffset(2, -2)
 
