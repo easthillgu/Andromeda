862b9e1f7b1d86814947e5979b7e94f5e7480293
easthillgu <58293755@qq.com>
2026-05-28 22:30:48 +0800

    chore(theme): 清理 Retail-only 主题文件，精简 Blizzard UI 注册

--- modules/theme/blizzard/Blizzard_ObliterumUI.lua
+++
@@ -1,9 +0,0 @@
-local F, C = unpack(select(2, ...))
-
-C.Themes['Blizzard_ObliterumUI'] = function()
-    local obliterum = _G.ObliterumForgeFrame
-
-    F.ReskinPortraitFrame(obliterum)
-    F.ReskinButton(obliterum.ObliterateButton)
-    F.ReskinIcon(obliterum.ItemSlot.Icon)
-end
